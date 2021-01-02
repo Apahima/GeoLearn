9953ad3256754c54faaaef82daac6929811e8929
Adi Pahima <fahimaadi@gmail.com>
2021-01-02 12:09:17 +0200

    HW2 - Adding coordinate permutation Q3

--- HW2/main.py
+++ HW2/main.py
@@ -3,8 +3,8 @@ from HW2 import Q3, Q5
 
 if __name__ == '__main__':
     # --- Question 3 --- ##
-    # Q3.SectionOne()
-    # Q3.SectionTwo()
+    Q3.SectionOne()
+    Q3.SectionTwo()
     Q3.SectionThree()
     #
     # # --- Question 5 --- ##
